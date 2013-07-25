8f11ceb51808db7459eda58f2f99d2d1c3cfff0c
ben sunderland <ben.sunderland22@gmail.com>
2013-07-25 16:22:20 +1000

    tidy up

--- createSubjects/src/sample/tibbr/api/SampleTibbrMethod.java
+++ createSubjects/src/sample/tibbr/api/SampleTibbrMethod.java
@@ -1,9 +1,3 @@
-/*
- * 
- *   Creates a user in tibbr (server specified at bottom) using local csv file
- *   User is created as deactivated - go in as admin to activate
- */
-
 package sample.tibbr.api;
 
 import java.io.BufferedReader;
@@ -27,6 +21,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.w3c.dom.Document;
@@ -40,54 +35,7 @@ import au.com.bytecode.opencsv.CSVReader;
 
 public class SampleTibbrMethod {
 	
-	public String getUsername() {
-		return username;
-	}
-
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-
-	public String getPassword() {
-		return password;
-	}
-
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-
-	public String getUrlBase() {
-		return urlBase;
-	}
-
-
-	public void setUrlBase(String urlBase) {
-		this.urlBase = urlBase;
-	}
-
-
-	public String getAuth_token() {
-		return auth_token;
-	}
-
-
-	public void setAuth_token(String auth_token) {
-		this.auth_token = auth_token;
-	}
-
-
-	public String getClient_key() {
-		return client_key;
-	}
-
-
-	public void setClient_key(String client_key) {
-		this.client_key = client_key;
-	}
+	
 
 
 	private String username;
@@ -106,27 +54,35 @@ public SampleTibbrMethod(String _urlbase, String _username, String _password){
 		
 }
 
-
+// logs in as specified user and sets the class member "auth_token" which is needed for all subsequent API calls
 public void loginUser() throws UnsupportedEncodingException{
 	
 	DefaultHttpClient httpClient = new DefaultHttpClient();
-	//System.out.println("URL: "+urlBase+"a/users/login.xml");
+	System.out.println("URL: "+urlBase+"/a/users/login.xml");  // THIS IS A POST
+	System.out.println("client_key: " + this.client_key); 
 	HttpPost postRequest = new HttpPost(urlBase+"/a/users/login.xml");
+	
+	
 	StringEntity input = new StringEntity("params[login]=" + URLEncoder.encode(this.username,"UTF-8") + 
             							  "&params[password]=" + URLEncoder.encode(this.password,"UTF-8") + 
             							  "&client_key=" + URLEncoder.encode(client_key,"UTF-8"));
 
 	input.setContentType("application/x-www-form-urlencoded");
-	postRequest.setEntity(input);
+	postRequest.setEntity(input); // attach params to request
 	
+	String body;
 	try {
-		HttpResponse response=httpClient.execute(postRequest);
-		String body = readStream(response.getEntity().getContent());
+		HttpResponse response = httpClient.execute(postRequest); // execute POST
+		body = readStream(response.getEntity().getContent());
 		Document user = parseXml(body);
-        Node auth_token_node = firstElementByTag(user, "auth-token");
+	    Node auth_token_node = firstElementByTag(user, "auth-token");
 		this.auth_token=auth_token_node.getNodeValue();
-        
+		
+		
 	} catch (ClientProtocolException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IllegalStateException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	} catch (IOException e) {
@@ -134,15 +90,45 @@ public void loginUser() throws UnsupportedEncodingException{
 		e.printStackTrace();
 	}
 	
+	
+	
+	
 }
 
+
+public void getAllUsers(){
+	
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+		String params = "?client_key="+this.client_key + "&auth_token="+this.auth_token;
+		HttpGet getRequest = new HttpGet(urlBase+"/a/users/1/search_users.xml"+params);
+		
+		System.out.println("auth_token: " + this.auth_token); 
+		
+		try {
+			HttpResponse response = httpClient.execute(getRequest);
+			
+			String body = readStream(response.getEntity().getContent());
+			//Document user = parseXml(body);
+			System.out.println(body);
+			
+	        
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	
+}
 
 
 public void createMessages(String message){
 	
 	URL server;
 	try {
-		server = new URL(this.urlBase+"/a"+"/messages.xml");
+		server = new URL(this.urlBase+"/a"+"/messages.xml");   // POST
 		
 		HttpURLConnection conn= (HttpURLConnection)server.openConnection();
 		conn.setDoOutput(true);
@@ -314,9 +300,9 @@ String readStream(InputStream is) throws IOException {
 
 Document parseXml(String xml) {
     InputSource is = new InputSource(new StringReader(xml));
-    DocumentBuilderFactory builderFactory =
-            DocumentBuilderFactory.newInstance();
+    DocumentBuilderFactory builderFactory =  DocumentBuilderFactory.newInstance();
     DocumentBuilder builder = null;
+    
     try {
             builder = builderFactory.newDocumentBuilder();
     } catch (ParserConfigurationException e) {
@@ -353,15 +339,15 @@ Node firstElementByTag(Document d, String tag) {
 
 public static void main(String[] args) {
 	
-	SampleTibbrMethod tibbr = new SampleTibbrMethod("http://172.16.101.129/","tibbradmin","password");
-
+	SampleTibbrMethod tibbr = new SampleTibbrMethod("https://tibbrdemo.tibbr.com","bsunderl","password");
 
  	
 	try {
 		tibbr.loginUser();
 		//tibbr.createUsersFromFile("sample_users");
-		tibbr.createSubjectsFromFile("sample_subjects");
+		//tibbr.createSubjectsFromFile("sample_subjects");
 		
+		tibbr.getAllUsers();
 		
 		
 	} catch (Exception e) {
